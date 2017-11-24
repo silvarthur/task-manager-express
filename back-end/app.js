68c937d6b3a4cc8f8f33782a4f876acaeb1214b4
Arthur Antunes <arthuragds@gmail.com>
2017-11-23 23:27:43 -0300

    Adding connection to the BD in the project.

--- back-end/app.js
+++ back-end/app.js
@@ -4,9 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+mongoose.connect('mongodb://localhost/task_manager_db');
+var db = mongoose.connection;
+
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+db.on('error', function(err) {
+  console.log(err);
+});
 
 var app = express();
 
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Task = require('../models/task');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Angular Task Manager' 
+  Task.find({}, function(err, tasks){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('index', { 
+        title: 'Angular Task Manager',
+        tasks: tasks 
+      });
+    }
   });
+
 });
 
 module.exports = router;
