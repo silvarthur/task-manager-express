d5e89255e90c1b4ab09f3e704f5c9117b828d47e
Arthur Antunes <arthuragds@gmail.com>
2017-12-03 15:36:22 -0300

    Improving the user UI on the task page.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
       console.log(err);
     } else {
       res.render('index', {
-        title: 'Angular Task Manager',
+        title: 'Express JS Task Manager',
         tasks: tasks 
       });
     }
