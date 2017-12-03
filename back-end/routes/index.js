d458b0c6657c610a7f621ef17a9f119cd1a7d48e
Arthur Antunes <arthuragds@gmail.com>
2017-12-03 14:02:37 -0300

    Removing useless code.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -21,10 +21,6 @@ router.get('/', function(req, res, next) {
 //EDIT TASK
 router.get('/:_id', function(req, res) {
   Task.findById(req.params._id, function(err, task) {
-    console.log("***");
-    console.log(req.params._id);
-    console.log("***");
-
     if(err) {
       console.log(err);
     } else {
