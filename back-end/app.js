4389dec81d65c0dd46198438936ade57be559cca
Arthur Antunes <arthuragds@gmail.com>
2017-12-08 20:39:11 -0300

    Adding express-session, connect-flash and express-validator to app.js file.

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -38,6 +42,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+//express messages middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//express validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+
+    return {
+      param: formParam,
+      msg: msg,
+      value : value
+    };
+  }
+}));
 
 app.use('/', index);
 app.use('/users', users);
