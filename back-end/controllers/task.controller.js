78affcb8f92d6d1a7b5cdea1891923460e4bd0a4
Arthur Silva <arthur.dias@embedded.ufcg.edu.br>
2017-12-14 11:42:42 -0200

    Working on task.controller.js and task.route.js.

--- back-end/controllers/task.controller.js
+++ back-end/controllers/task.controller.js
@@ -9,4 +9,37 @@ exports.getAllTasks = function(callback) {
           callback(tasks);
         }
       });
-}
+};
+
+exports.addNewTask = function(callback) {
+    var task = new Task();
+    task.title = req.body.title;
+    task.description = req.body.description;
+  
+    task.save(function(err, result) {
+        if(err) {
+            callback(err);
+        } else {
+            callback(200); //HTTP STATUS 200 OK
+        }
+    });
+};
+
+exports.updateTask = function(req, callback) {
+    Task.findOne({_id: req.params._id, function(err, task) {
+        if(err) {
+            res.json({err: 'Task could not be found!'});
+        } else {
+            if(req.body.title) task.title = req.body.title;
+            if(req.body.description) task.description = req.body.description;
+
+            task.save(function(error, task) {
+                if(error) {
+                    callback({error: 'The could not be saved!'});
+                } else {
+                    callback(task);
+                }
+            });
+        }
+    }});
+};
--- back-end/routes/task.route.js
+++ back-end/routes/task.route.js
@@ -13,57 +13,20 @@ router.get('/', function(req, res) {
     });
 });
 
-/*
-//ADD NEW TASK
+//ADD NEW TASKS
 router.post('/add', function(req, res) {
-    var task = new Task();
-    task.title = req.body.title;
-    task.description = req.body.description;
-  
-    task.save(function(err, result) {
-        if(err) {
-            res.status(400).json(err);
-        } else {
-            res.redirect('/');
-            //res.json(result);
-        }
+    controller.addNewTask(req, function(response) {
+        res.json(response);
     });
 });
-  
-//LOAD EDIT FORM
-router.get('/edit/:_id', function(req, res) {
-    Task.findById(req.params._id, function(err, task) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render('task', {
-            title: 'Task Details',
-            task_id: req.params._id,
-            task_title: task.title,
-            task_description: task.description
-        });
-    }
-    });
-});
-  
+
 //EDIT TASK
-router.post('/edit/:_id', function(req, res) {
-    var task = {};
-    task.title = req.body.title;
-    task.description = req.body.description;
-
-    var query = {_id: req.params._id};
-
-    Task.update(query, task, function(err) {
-        if(err) {
-            console.log(err);
-            return;
-        } else {
-            console.log('Task successfully edited!');
-            res.redirect('/');
-        }
+router.put('/edit/:_id', function(req, res) {
+    controller.updateTask(req, function(response) {
+        res.json(response);
     });
 });
-*/
+
+//REMOVE TASK
 
 module.exports = router;
