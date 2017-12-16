8bdc8402773634c122a972153054bca428c9ea91
Arthur Antunes <arthuragds@gmail.com>
2017-12-16 15:30:45 -0300

    Creating home and task modules.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
