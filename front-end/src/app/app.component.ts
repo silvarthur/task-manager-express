b59867db6e504da888da31c87fa5ff3a13e8cd6e
Arthur Silva <arthur.dias@embedded.ufcg.edu.br>
2018-01-17 12:46:55 -0200

    Adding redirect function to the home page component file.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {}
+
+  redirect(path): void {
+    this.router.navigate([path], { relativeTo: this.route })
+    .then(data => console.log(data));
+  }
 }
